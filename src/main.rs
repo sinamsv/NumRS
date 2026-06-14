b1f69b7c0bc16eab16fa4162b4fca4018c4c4d1e
sinamsv <sinamsv0@gmail.com>
2026-06-14 20:50:21 +0330

    first version of NumRS

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,10 @@ fn main() {
         [1,  0],
         [-2, 3],
         [4, -1]
-    ];
-    
-    let c = ns_array![
-        [5, 6, 9],
-        [7, 10, 9]
-    ];
-    
+    ];    
     println!("A:\n{}", a);
     println!("B:\n{}", b);
-    println!("C:\n{}", c);
-    println!("A × B × C:\n{}", a.clone() * b * c);
+    println!("A × B × C:\n{}", a.clone() * b);
 
     let x = ns_array![[1, 2, 3], [4, 5, 6]];
     let y = ns_array![[6, 5, 4], [3, 2, 1]];
